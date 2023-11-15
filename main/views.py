f887cd3a725cd0cee3a0e4882fd23f317867aa28
nabiilahputri13 <putrisafa1394@gmail.com>
2023-11-15 14:47:20 +0700

    tutor8

--- main/views.py
+++ main/views.py
@@ -9,8 +9,28 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages  
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+    
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
